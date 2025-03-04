b7bd1c292655377192370c6d8e7af7241396172a
Riniel Rodrigo <rinielrodrigo555@gmail.com>
2025-03-04 18:50:09 -0300

    feat: create, config models vehicle and consum

---
+++ fuel-manager-api/Models/Consum.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace fuel_manager_api.Models
+{
+    [Table("Consum")]
+    public class Consum
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Desc { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; }
+
+        [Required]
+        public decimal Value { get; set; }
+
+        [Required]
+        public FuelType Type { get; set; }
+
+        [Required]
+        public int VeiculoId { get; set; }
+
+        public Vehicle Vehicle { get; set; }
+
+    }
+
+    public enum FuelType
+    {
+        Gasoline,
+        Alcohol,
+        Diesel
+    }
+
+
+}
---
+++ fuel-manager-api/Models/Vehicle.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace fuel_manager_api.Models
+{
+    [Table("Vehicle")]
+    public class Vehicle
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Mark { get; set; }
+
+        [Required]
+        public string Model { get; set; }
+
+        [Required]
+        public string Plate { get; set; }
+
+        [Required]
+        public int YearFab { get; set; }
+
+        [Required]
+        public int YearModel { get; set; }
+
+        [Required]
+        public ICollection<Consum> Consums { get; set; }
+    }
+}
